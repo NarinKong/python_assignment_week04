30e10a865eafb8e722ddd7ab6bb1af6983c963a1
NarinKong <narinkongpithaksilp@gmail.com>
2025-07-22 12:24:30 +0700

    NK

---
+++ wk04-6.py
@@ -0,0 +1,4 @@
+grades = [55, 70, 65, 40, 90, 85, 50, 77]
+passed_with_bonus = list(filter(lambda x: x>=60, grades))
+passed_with_bonus = [x*1.05 for x in passed_with_bonus]
+print(passed_with_bonus)
